9678dfee34bbcb42dcaec99ffa09855b1c8e1745
gavin <gavinsutton6511@gmail.com>
2023-09-18 15:42:59 +0900

    Add sticky header to index page

--- index.js
+++ index.js
@@ -37,3 +37,22 @@ window.addEventListener('click', function(event) {
         }
     }
 })
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+let header = document.getElementById("index-header");
+
+// Get the offset position of the navbar
+let sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
